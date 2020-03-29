1883a6b7d024374ed637e88d8c8cbd2e09bc1982
Forestas <56953424+Yan420@users.noreply.github.com>
2020-03-29 15:55:17 +0200

    Add files via upload

---
+++ Heure.java
@@ -0,0 +1,27 @@
+
+public class Heure {
+	private int heure, minute;
+
+	public void setHeure(int heure) {
+		this.heure = heure;
+	}
+
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+
+	public int getHeure() {
+		return heure;
+	}
+
+	public int getMinute() {
+		return minute;
+	}
+
+	public Heure(int heure, int minute) {
+		super();
+		this.heure = heure;
+		this.minute = minute;
+	}
+	
+}
